70da0a70cb79fb09ccdcb0aca40ea8182650db67
abderrhmane ben aicha <DAHMANGOT2020@GMAIL.COM>
2024-08-28 13:46:21 +0100

    fix build v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,9 @@
 import CountdownTimer from "@/components/CountdownTimer";
-import ProductImageSlider from "@/components/ProductImageSlider";
-import ProductList from "@/components/ProductList";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    // <main className="mt-20 w-full p-2">
-    //   {/* Welcome Section */}
-    //   <section className="p-2 text-center">
-    //     <h1 className="text-3xl md:text-4xl font-bold mb-4">
-    //       مرحبًا بكم في موقعنا
-    //     </h1>
-    //     <p className="text-lg md:text-xl text-gray-600">
-    //       استعرض أحدث المنتجات والعروض الخاصة بنا!
-    //     </p>
-    //   </section>
-
-    //   {/* Image Slider */}
-    //   <ProductImageSlider />
-
-    //   {/* Product List Section */}
-    //   <section className="mt-8">
-    //     <h2 className="text-2xl font-bold mb-4">جميع المنتجات</h2>
-    //     <ProductList />
-    //   </section>
-    // </main>
     <main>
       <div className="p-4">
         <h1 className="text-center font-extrabold text-2xl p-4">مرحبًا بكم في موقعنا</h1>
